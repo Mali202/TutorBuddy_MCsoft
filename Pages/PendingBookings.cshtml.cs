c5ca1f7a5677d8536c907abdf0095e7762a01b1c
Mali Matiso <s220651337@mandela.ac.za>
2021-10-07 12:48:36 +0200

    jii

---
+++ Pages/PendingBookings.cshtml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using TutorBuddy_MCsoft.Areas.Identity.Data;
+using TutorBuddy_MCsoft.Data;
+using TutorBuddy_MCsoft.Models;
+
+namespace TutorBuddy_MCsoft.Pages
+{
+    public class PendingBookingsModel : PageModel
+    {
+        private readonly TutorBuddy_MCsoft.Data.TutorBuddy_MCsoftContext _context;
+        private readonly UserManager<TutorBuddy_MCsoftUser> _usermanager;
+
+        public PendingBookingsModel(TutorBuddy_MCsoft.Data.TutorBuddy_MCsoftContext context, UserManager<TutorBuddy_MCsoftUser> userManager)
+        {
+            _context = context;
+            _usermanager = userManager;
+        }
+
+        public IList<IndividualBooking> IndividualBooking { get;set; }
+
+        public async Task OnGetAsync(int? id)
+        {
+            TutorBuddy_MCsoftUser user = await _usermanager.GetUserAsync(User);
+            IndividualBooking = await _context.IndividualBookings.Where(ib => ib.Session.ModuleTutor.Tutor.StudentNumber == id).ToListAsync();
+        }
+    }
+}

85a87c774e0d5bc55748a201efd2e8fdc7c78d74
Mali Matiso <s220651337@mandela.ac.za>
2021-10-28 12:28:59 +0200

    dddgds

---
+++ Pages/Modules/Maintain.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using TutorBuddy_MCsoft.Data;
+using TutorBuddy_MCsoft.Models;
+
+namespace TutorBuddy_MCsoft.Pages.Modules
+{
+    public class MaintainModel : PageModel
+    {
+        private readonly TutorBuddy_MCsoft.Data.TutorBuddy_MCsoftContext _context;
+
+        public MaintainModel(TutorBuddy_MCsoft.Data.TutorBuddy_MCsoftContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Module> Module { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Module = await _context.Modules.ToListAsync();
+        }
+    }
+}
---
+++ Pages/RateTutor.cshtml.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using TutorBuddy_MCsoft.Data;
+using TutorBuddy_MCsoft.Models;
+
+namespace TutorBuddy_MCsoft.Pages
+{
+    public class RateTutorModel : PageModel
+    {
+        private readonly TutorBuddy_MCsoftContext _context;
+        private readonly Use_Cases use_;
+
+        public RateTutorModel(TutorBuddy_MCsoftContext context)
+        {
+            _context = context;
+            use_ = new Use_Cases(_context);
+        }
+
+        public Tutor tutor { get; set; }
+        public Student student { get; set; }
+        [BindProperty]
+        public string stars { get; set; }
+        [BindProperty]
+        public string comment { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? ts, int? ss)
+        {
+            tutor = await _context.Tutors.FirstOrDefaultAsync(t => t.StudentNumber == ts);
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? ts, int? ss)
+        {
+            tutor = await _context.Tutors.FirstOrDefaultAsync(t => t.StudentNumber == ts);
+            student = await _context.Student.FirstOrDefaultAsync(s => s.StudentNumber == ss);
+            double rating = stars != null ? double.Parse(stars) : 0;
+            Review review = new() { Rating = rating, Comment = comment, Student = student, Tutor = tutor };
+            //use_.addReview(review);
+            return RedirectToPage("./Index");
+        }
+    }
+}

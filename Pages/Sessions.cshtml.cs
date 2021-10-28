c690c957bf16c21376b1b603872c06758276629d
Mali Matiso <s220651337@mandela.ac.za>
2021-10-28 12:54:19 +0200

    rgrs

---
+++ Pages/Sessions.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using TutorBuddy_MCsoft.Data;
+using TutorBuddy_MCsoft.Models;
+
+namespace TutorBuddy_MCsoft.Pages
+{
+    public class SessionsModel : PageModel
+    {
+        private readonly TutorBuddy_MCsoft.Data.TutorBuddy_MCsoftContext _context;
+
+        public SessionsModel(TutorBuddy_MCsoft.Data.TutorBuddy_MCsoftContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Session> Session { get;set; }
+
+        public async Task OnGetAsync(int? ss)
+        {
+            IList<IndividualBooking> bookings = await _context.IndividualBookings.Where(ib => ib.Student.StudentNumber == ss).ToListAsync(); 
+            Session = bookings.Select(ib => ib.Session).ToList();
+        }
+    }
+}

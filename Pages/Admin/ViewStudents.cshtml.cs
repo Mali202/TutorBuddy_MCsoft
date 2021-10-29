4338aedc43f9f2a16f10f217e30e8315666b7829
Mali Matiso <s220651337@mandela.ac.za>
2021-10-29 11:21:52 +0200

    ukg

---
+++ Pages/Admin/ViewStudents.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using TutorBuddy_MCsoft.Data;
+using TutorBuddy_MCsoft.Models;
+
+namespace TutorBuddy_MCsoft.Pages
+{
+    public class ViewStudentsModel : PageModel
+    {
+        private readonly TutorBuddy_MCsoft.Data.TutorBuddy_MCsoftContext _context;
+
+        public ViewStudentsModel(TutorBuddy_MCsoft.Data.TutorBuddy_MCsoftContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Student> Student { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Student = await _context.Student.ToListAsync();
+        }
+    }
+}

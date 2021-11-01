8a69ece9d678be66b599f4b46adaac9093e1cc0d
Mali Matiso <s220651337@mandela.ac.za>
2021-11-01 12:11:21 +0200

    kogjfj

--- Pages/Modules/Index.cshtml.cs
+++ Pages/Modules/Index.cshtml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AspNetCoreHero.ToastNotification.Abstractions;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
@@ -13,10 +15,12 @@ namespace TutorBuddy.Pages.Modules
     public class IndexModel : PageModel
     {
         private readonly TutorBuddy_MCsoftContext _context;
+        private readonly INotyfService _notyf;
 
-        public IndexModel(TutorBuddy_MCsoftContext context)
+        public IndexModel(TutorBuddy_MCsoftContext context, INotyfService notyf)
         {
             _context = context;
+            _notyf = notyf;
         }
 
         public List<Module> Module { get;set; }
@@ -26,11 +30,11 @@ namespace TutorBuddy.Pages.Modules
             Module = await _context.Modules.ToListAsync();
             if(sort != null)
             {
-                IList<Session> sessions = await _context.Sessions.ToListAsync();
-                foreach (Module item in Module)
-                {
-                    int count = sessions.Count(s => s.ModuleTutor.ModuleID == item.ModuleID);
-                }
+                IList<Session> sessions = await _context.Sessions.Include(s => s.ModuleTutor).ToListAsync();
+                //IComparer<Module> comparer = new i
+                Module = Module.OrderByDescending(m => sessions.Count(s => s.ModuleTutor.ModuleID.Equals(m.ModuleID))).ToList();
+                //Module.Sort((m1, m2) 
+                //    => sessions.Count(s => s.ModuleTutor.Module.ModuleCode.Equals(m1.ModuleCode)) - sessions.Count(s => s.ModuleTutor.Module.ModuleCode.Equals(m2.ModuleCode)));
             }
             
         }
